933dd153845f2a55bd7bed4a8f1b87bfd4e9899f
Tomas <tomas.ew88@gmail.com>
2024-05-02 21:14:04 +0300

    Browse page

---
+++ src/app/_components/CourseCard.tsx
@@ -0,0 +1,16 @@
+type CourseCardProps = {
+  title: string;
+  url: string;
+}
+
+export default function CourseCard(props: CourseCardProps) {
+  return (
+    <a href={props.url} className="flex flex-col w-80 h-40 border-2 rounded-md">
+      <div className="text-lg">{props.title}</div>
+      <div className="w-full h-[2px] bg-gray-200"></div>
+      <div className="bg-green-200 flex-1">
+        <p>Image</p>
+      </div>
+    </a>
+  );
+}
