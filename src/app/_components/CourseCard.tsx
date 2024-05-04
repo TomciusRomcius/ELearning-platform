fe0854519450d79c7a4fa22ec863d11cfbca645a
Tomas <tomas.ew88@gmail.com>
2024-05-04 13:20:46 +0300

    Render course cards

--- src/app/_components/CourseCard.tsx
+++ src/app/_components/CourseCard.tsx
@@ -5,7 +5,7 @@ type CourseCardProps = {
 
 export default function CourseCard(props: CourseCardProps) {
   return (
-    <a href={props.url} className="flex flex-col w-80 h-40 border-2 rounded-md">
+    <a href={`/courses/${props.url}`} className="flex flex-col w-80 h-40 border-2 rounded-md">
       <div className="text-lg">{props.title}</div>
       <div className="w-full h-[2px] bg-gray-200"></div>
       <div className="bg-green-200 flex-1">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+"use client"
+import { useEffect, useState } from "react";
 import CourseCard from "./_components/CourseCard";
+import axios from "axios";
 
 export default function Page() {
+  let [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    axios.get("api/courses")
+      .then((courses) => {
+        setCourses(courses.data);
+        console.log(courses.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
   return (
     <main className="flex flex-col gap-10">
       <header className="flex justify-between px-20 py-5 items-center border-b-2 border-gray-200">
@@ -16,10 +32,7 @@ export default function Page() {
       <section className="flex flex-col gap-10 px-60">
         <h1 className="text-6xl text-center">Browse</h1>
         <div className="flex flex-wrap justify-start gap-10">
-          <CourseCard url="course" title="Marketing"/>
-          <CourseCard url="course" title="Marketing"/>
-          <CourseCard url="course" title="Marketing"/>
-          <CourseCard url="course" title="Marketing"/>
+          {courses.map((course) => <CourseCard url={course?._id} title={course?.title}/>)}
         </div>
       </section>
     </main>
