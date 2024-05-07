b6012762bbd54d45f1b6907b9212b839cd07abf2
Tomas <tomas.ew88@gmail.com>
2024-05-07 13:13:31 +0300

    Block classes

--- src/app/courses/[...id]/ui/BlockTypeSelector.tsx
+++ src/app/courses/[...id]/ui/BlockTypeSelector.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Image from "next/image";
+import Popup from "@/ui/Popup";
 
 type BlockTypeSelectorProps = {
   setType: (type: string) => void;
@@ -11,19 +12,24 @@ export default function BlockTypeSelector(props: BlockTypeSelectorProps) {
   const onToggle = () => setPopupVisible(!popupVisible);
   const onSelect = (type: string) => {
     props.setType(type);
+    setPopupVisible(false);
   }
   return (
-    <button onClick={onToggle}>
+    <button className="relative" onClick={onToggle}>
       <Image
         src="/blockTypeSelector.svg"
         width={24}
         height={24}
         alt=""/>
       {popupVisible ? (
-        <div className="flex flex-col gap-2 items-start p-4 bg-white shadow-lg rounded-lg absolute">
-          <button onClick={() => onSelect("paragraph")}>Paragraph</button>
-          <button onClick={() => onSelect("h1")}>Header 1</button>
-        </div>
+        <Popup isFixed={false}  onClose={() => setPopupVisible(false)}>
+          <div className="flex flex-col gap-2 items-start p-4 bg-white shadow-lg rounded-lg">
+            <button onClick={() => onSelect("paragraph")}>Paragraph</button>
+            <button onClick={() => onSelect("h1")}>Header 1</button>
+            <button onClick={() => onSelect("h2")}>Header 2</button>
+            <button onClick={() => onSelect("h3")}>Header 3</button>
+          </div>
+        </Popup>
       ) : null}
     </button>
   );
--- src/app/courses/[...id]/utils/blocks.ts
+++ src/app/courses/[...id]/utils/blocks.ts
@@ -1,4 +1,13 @@
-const blockClasses = {
+export const blockClasses: Map<string, string> = new Map();
+
+blockClasses.set("paragraph", "text-sm");
+blockClasses.set("h1", "text-4xl");
+blockClasses.set("h2", "text-2xl");
+blockClasses.set("h3", "text-xl");
+
+/*
   paragraph: "text-sm",
-  h1: "text-2xl",
-}
+  h1: "text-4xl",
+  h2: "text-2xl",
+  h3: "text-xl",
+*/
