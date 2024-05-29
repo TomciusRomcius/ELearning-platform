7e0d9a42c0ffc1302b4861bb862dbc36feb027c1
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-05-29 16:53:01 +0300

    Use the new editor manager. Note: this currently doesn't support lessons

--- src/app/courses/[...id]/hooks/useCourse.tsx
+++ src/app/courses/[...id]/hooks/useCourse.tsx
@@ -1,15 +1,18 @@
-import axios from "axios";
-import { redirect, useParams } from "next/navigation";
+"use client"
+
+import { useParams } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { CourseType, CurrentLessonType, LessonType } from "../utils/types";
+import CourseEditorManager from "@/services/courseEditorManager";
 
 export function useCourse() {
-  let [course, setCourse] = useState<CourseType>();
-  let [currentLessonId, setCurrentLessonId] = useState<string>();
-  let currentModuleId = useRef<string | null>();
+  let [course, setCourse] = useState<CourseType | null>(null);
+  let [currentLessonId, setCurrentLessonId] = useState<string>("");
+  let currentModuleId = useRef<string | null>("");
   const params = useParams();
 
   const getLesson = (): CurrentLessonType | null => {
+    if (!currentLessonId) return null;
     const module = course?.modules.find(
       (element) => element._id === currentModuleId.current
     );
@@ -31,19 +34,24 @@ export function useCourse() {
 
   let lesson: CurrentLessonType | null = getLesson();
 
+  // Note we must currently use structured clone to change the reference of the course object
+  // to allow it to update. It's not ideal.
   useEffect(() => {
-    axios
-      .get(`/api/courses/${params.id}`)
-      .then((apiCourse) => {
-        setCourse(apiCourse.data);
+    CourseEditorManager.fetchCourse(params.id as string)
+      .then(() => {
+        if (!CourseEditorManager.currentCourse) return;
+        setCourse(structuredClone(CourseEditorManager.currentCourse));
       })
-      .catch((error) => {
-        redirect("./");
-      });
 
-    setCurrentLessonId(course?.modules[0].lessons[0]._id);
-    currentModuleId.current = course?.modules[0]._id;
+    CourseEditorManager.subscribers.push(() => {
+      if (!CourseEditorManager.currentCourse) return;
+      setCourse(structuredClone(CourseEditorManager.currentCourse));
+    });
+
+    // setCurrentLessonId(course?.modules[0].lessons[0]._id);
+    // currentModuleId.current = course?.modules[0]._id;
   }, []);
 
+  console.log(course);
   return { setCurrentLesson, course, lesson };
 }
