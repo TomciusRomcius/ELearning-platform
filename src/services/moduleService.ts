f8081509a565a6d4de9d932ca9ffd566b267b81e
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-05-29 16:54:04 +0300

    Update modules in patches

--- src/services/moduleService.ts
+++ src/services/moduleService.ts
@@ -12,3 +12,14 @@ export async function createModule(courseId: string, module: ModuleType) {
 export async function deleteModule(courseId: string, moduleId: string) {
   await axios.delete(`/api/courses/${courseId}/modules/${moduleId}`);
 }
+
+// We must have an _id assotiated with the module!
+export async function updateModule(courseId: string, module: ModuleType) {
+  if (!module._id)
+    throw new Error("Module doesn't containt an id!");
+  axios.patch(`/api/courses/${courseId}/modules/${module._id}`, {
+    module: {
+      moduleName: module.moduleName,
+    },
+  })
+}
