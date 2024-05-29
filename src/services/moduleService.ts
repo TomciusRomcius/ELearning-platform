5a36d36ccf9c3cdbb5f962b65291f1c84ef70538
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-05-29 16:49:38 +0300

    Breaking: Create module service passes a module now instead of Module name

--- src/services/moduleService.ts
+++ src/services/moduleService.ts
@@ -1,9 +1,12 @@
+import { ModuleType } from "@/app/courses/[...id]/utils/types";
 import axios from "axios";
 
-export async function createModule(courseId: string, moduleName: string) {
-  await axios.post(`/api/courses/${courseId}/modules`, {
-    moduleName: moduleName,
+export async function createModule(courseId: string, module: ModuleType) {
+  let req = await axios.post(`/api/courses/${courseId}/modules`, {
+    module: module,
   });
+
+  return req;
 }
 
 export async function deleteModule(courseId: string, moduleId: string) {
