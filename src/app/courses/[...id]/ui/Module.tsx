4bbddc27b18f12281d2f1b53a318e6701e96dd0b
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-05-29 16:53:24 +0300

    Rename module

--- src/app/courses/[...id]/ui/Module.tsx
+++ src/app/courses/[...id]/ui/Module.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { LessonType, ModuleType } from "../utils/types";
 import LessonButton from "./LessonButton";
 import NewLessonButton from "./NewLessonButton";
 import Image from "next/image";
 import Popup from "@/ui/Popup";
-import { deleteModule } from "@/services/moduleService";
+import CourseEditorManager from "@/services/courseEditorManager";
 
 type ModuleProps = {
   courseId: string;
@@ -14,13 +14,28 @@ type ModuleProps = {
 
 export function Module(props: ModuleProps) {
   let [popupVisible, setPopupVisible] = useState(false);
+  let [isRenaming, setIsRenaming] = useState(false);
+  let nameRef = useRef<HTMLInputElement>(null);
 
   const onTogglePopup = () => {
     setPopupVisible(!popupVisible);
   }
 
+  const onToggleRename = () => {
+    setIsRenaming(!isRenaming);
+  }
+
+  const onRename = () => {
+    let newName = nameRef.current?.value;
+    if (!newName) return;
+    let newModule = structuredClone(props.module);
+    newModule.moduleName = newName;
+    CourseEditorManager.updateModule(newModule);
+    setIsRenaming(false);
+  }
+
   const onDelete = () => {
-    deleteModule(props.courseId, props.module._id);
+    CourseEditorManager.deleteModule(props.module._id || "");
   }
 
   return (
@@ -29,13 +44,21 @@ export function Module(props: ModuleProps) {
         <Popup onClose={onTogglePopup} isFixed={false}>
           <div className="text-text-grayed flex flex-col gap-2 items-start p-4 bg-primary-200 border-2 border-border shadow-lg rounded-lg">
             <button onClick={onDelete}>Delete</button>
+            <button onClick={onToggleRename}>Rename</button>
           </div>
         </Popup>
       ) : null}
       <div className="flex gap-2">
-        <h1 className="text-text-light text-2xl">
-          Module: {props.module.moduleName}
-        </h1>
+        {!isRenaming ? (
+          <h1 className="text-text-light text-2xl">
+            Module: {props.module.moduleName}
+          </h1>
+        ) : (
+          <div className="flex gap-2">
+            <input ref={nameRef} defaultValue={props.module.moduleName} />
+            <button onClick={onRename}>Submit</button>
+          </div>
+        )}
         <button onClick={onTogglePopup}>
           <Image src="/moreHorizontal.svg" width={24} height={24} alt="" />
         </button>
