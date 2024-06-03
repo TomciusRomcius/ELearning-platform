89bf5893fc777dfb1492f1cb89db4af8e04c58b9
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-03 10:40:36 +0300

    Default user role

--- src/backend/controllers/userController.ts
+++ src/backend/controllers/userController.ts
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
-import { UserModel } from "../models/userModel";
+import { UserModel, UserRole } from "../models/userModel";
 
 export async function createUser(email: any, password: any): Promise<void> {
   try {
-    let user = await new UserModel({ email, password });
+    let user = await new UserModel({ 
+      email: email,
+      password: password,
+      role: UserRole.NORMAL, 
+    });
     user.save();
   } catch (error) {
     console.log(error);
