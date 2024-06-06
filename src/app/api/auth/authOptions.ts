08d27d6be1408821d396348ce2f18ced0c5e6f98
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-06 15:20:34 +0300

    Include user role and id in the JWT token

--- src/app/api/auth/authOptions.ts
+++ src/app/api/auth/authOptions.ts
@@ -1,10 +1,34 @@
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, User } from "next-auth";
 import { signIn } from "../../../backend/controllers/userController";
 
+export interface CustomUser extends User {
+  role: number;
+  id: string;
+}
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        const customUser = user as CustomUser;
+        token.id = customUser.id;
+        token.role = customUser.role;
+      }
+      return token;
+    },
+
+    session: async ({ session, user, token }) => {
+      // Send properties to the client, like an access_token and user id from a provider.
+      if (session.user) {
+        session.user.id = token.id;
+        session.user.role = token.role;
+      }
+      return session
+    }
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -28,11 +52,12 @@ export const authOptions: NextAuthOptions = {
         const email = credentials?.email;
         const password = credentials?.password;
         if (!email || !password) return null;
-        const res = await signIn(email, password);
-        if (res) {
+        const id = await signIn(email, password);
+        if (id) {
           let user = { 
-            usernaemailme: email,
-            password: password
+            email: email,
+            password: password,
+            id: id,
           }
           return user;
         }
