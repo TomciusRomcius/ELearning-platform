b0fda3604b156ba277974a26ac72c456a5ab0ee0
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-11 12:52:15 +0300

    Layout

---
+++ src/app/courses/[...id]/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/app/globals.css";
+import SessionContainer from "@/frontend/ui/SessionContainer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-Learning plaftform",
+  description: "E-Learning plaftform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-background flex flex-col gap-10`}>
+        <SessionContainer>
+          <>
+            {children}
+          </>
+        </SessionContainer>
+      </body>
+    </html>
+  );
+}
