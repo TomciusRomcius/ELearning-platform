2b94df23df2f4c741195819a029cdad042c407ce
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-14 12:10:01 +0300

    Remove alert()

--- src/frontend/services/courseEnroll.ts
+++ src/frontend/services/courseEnroll.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export async function courseEnroll(courseId: string) {
-  alert(courseId);
   const res = await axios.post(`/api/courses/enrolled-courses`, {
     courseId: courseId,
   });  
