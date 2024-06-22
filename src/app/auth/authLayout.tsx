c0b9ee6417c264bb6977b50583776868d0b83191
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-22 12:33:13 +0300

    Add dark bg

--- src/app/auth/authLayout.tsx
+++ src/app/auth/authLayout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-screen min-h-screen flex flex-col items-center justify-center">
+    <div className="w-screen min-h-screen bg-primary-0 flex flex-col items-center justify-center">
       <div className="flex flex-col gap-4 w-1/4">
         {children}
       </div>
