08421e324deb4a8f1e4bb368ead69f08e646bf04
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-26 10:51:23 +0300

    Use new isAdmin util function

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,14 @@
-"use server"
+"use server";
 
-import { authOptions } from "../api/auth/authOptions"
+import { authOptions } from "../api/auth/authOptions";
 import { getServerSession } from "next-auth";
 import { navigate } from "@/utils/navigation";
 import { UserRole } from "@/backend/models/userModel";
-
+import { isAdmin } from "@/backend/utils/isAdmin";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
-  if (session?.user?.role === UserRole.ADMIN) {
+  if (await isAdmin()) {
     await navigate("/admin/courses");
-  }
-  else
-    await navigate("not-authorized");
+  } else await navigate("not-authorized");
 }
