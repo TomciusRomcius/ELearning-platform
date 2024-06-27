fc53016aab6b069e71c5d5f362d6408f7c855cf0
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-27 11:52:39 +0300

    Make the border thinner

--- src/app/admin/_layouts/SidebarLayout.tsx
+++ src/app/admin/_layouts/SidebarLayout.tsx
@@ -4,7 +4,7 @@ type SidebarLayoutProps = {
 
 export async function SidebarLayout(props: SidebarLayoutProps) {
   return (
-    <nav className="bg-foreground py-8 px-12 flex flex-col gap-4 items-start border-r-2 border-primary-300 w-1/6 h-screen">
+    <nav className="bg-foreground py-8 px-12 flex flex-col gap-4 items-start border-r-1 border-primary-300 w-1/6 h-screen">
       {props.children}
     </nav>
   )
