712463a91c2b8eb6602dfc49d77728eaa4f68808
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-30 16:12:49 +0300

    Format

--- src/app/admin/_layouts/BodyLayout.tsx
+++ src/app/admin/_layouts/BodyLayout.tsx
@@ -1,15 +1,11 @@
 type BodyLayoutProps = {
   children: React.ReactNode;
-}
+};
 
 export async function BodyLayout(props: BodyLayoutProps) {
   return (
     <div className="flex justify-center flex-1 h-screen">
-      <div className="h-full px-12 py-16 overflow-y-auto">
-
-      {props.children}
-      </div>
+      <div className="h-full px-12 py-16 overflow-y-auto">{props.children}</div>
     </div>
-  )
-  
+  );
 }
