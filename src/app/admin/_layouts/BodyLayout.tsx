5170e12bb8523e63fc69bf1f6c4042b9c078868e
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-30 16:11:25 +0300

    Change layout padding and add scroll

--- src/app/admin/_layouts/BodyLayout.tsx
+++ src/app/admin/_layouts/BodyLayout.tsx
@@ -4,8 +4,11 @@ type BodyLayoutProps = {
 
 export async function BodyLayout(props: BodyLayoutProps) {
   return (
-    <div className="flex justify-center flex-1 px-12 py-36 h-screen">
+    <div className="flex justify-center flex-1 h-screen">
+      <div className="h-full px-12 py-16 overflow-y-auto">
+
       {props.children}
+      </div>
     </div>
   )
   
