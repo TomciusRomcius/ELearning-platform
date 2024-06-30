b396e78503bc554e70ba2d00c35dac2b9e4f1312
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-06-30 16:22:23 +0300

    Fix TS errors with Session

---
+++ types/next-auth.d.ts
@@ -0,0 +1,14 @@
+import { UserRole } from "@/backend/models/userModel";
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      role: UserRole;
+      id: string;
+    };
+  }
+}
