74a6c795d9fd9fff8384e3aa89b95dcef2599771
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-07-03 09:09:10 +0300

    Scroll on sidebar

--- src/app/courses/[...id]/client/Sidebar.tsx
+++ src/app/courses/[...id]/client/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
   const { course } = useDataDetails();
 
   return (
-    <nav className="bg-foreground py-4 px-8 flex flex-col gap-4 border-r-1 border-primary-300 w-1/6 h-screen">
+    <nav className="w-1/6 h-screen overflow-y-auto bg-foreground py-4 px-8 flex flex-col gap-4 border-r-1 border-primary-300">
     <h2 className="text-text-light text-xl font-bold text-center">{course.title}</h2>
     {course.modules.map((module) => (
       <Module module={module}/>
