2b2a434bb62372d775976a74b5efa3c2312543c6
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-07-09 12:53:04 +0300

    Fix ui sizing

--- src/app/admin/_layouts/BodyLayout.tsx
+++ src/app/admin/_layouts/BodyLayout.tsx
@@ -4,8 +4,8 @@ type BodyLayoutProps = {
 
 export async function BodyLayout(props: BodyLayoutProps) {
   return (
-    <div className="flex justify-center flex-1 h-screen">
-      <div className="h-full px-12 py-16 overflow-y-auto">{props.children}</div>
+    <div className="flex justify-center flex-1 h-screen overflow-hidden px-24 py-36">
+      <div className="w-full h-full overflow-y-auto">{props.children}</div>
     </div>
   );
 }
