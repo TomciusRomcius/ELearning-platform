755cc1a71798d844621bba3194d4536c544bd253
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-07-12 12:05:02 +0300

    Added MainHeader to admin page

--- src/app/admin/_layouts/BodyLayout.tsx
+++ src/app/admin/_layouts/BodyLayout.tsx
@@ -4,7 +4,7 @@ type BodyLayoutProps = {
 
 export async function BodyLayout(props: BodyLayoutProps) {
   return (
-    <div className="flex justify-center flex-1 h-screen overflow-hidden px-24 py-36">
+    <div className="flex justify-center flex-1 h-full overflow-hidden px-24 py-36">
       <div className="w-full h-full overflow-y-auto">{props.children}</div>
     </div>
   );
