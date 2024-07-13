c03230adff97cdc24d74fd49ecf602bd73e9021e
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-07-13 13:18:18 +0300

    Provide a key

--- src/app/courses/[...id]/admin/AdminModule.tsx
+++ src/app/courses/[...id]/admin/AdminModule.tsx
@@ -56,6 +56,7 @@ export function AdminModule(props: ModuleProps) {
         {/* Display lessons */}
         {props.module.lessons.map((lesson) => (
           <LessonButton
+            key={lesson._id}
             courseId={props.courseId}
             setCurrentLesson={props.setCurrentLesson}
             moduleId={props.module._id}
