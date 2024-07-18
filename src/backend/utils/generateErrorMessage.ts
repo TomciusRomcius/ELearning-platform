29a1080b9471a897222cc63164dd9c9937ec2903
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-07-18 15:30:17 +0300

    Print error message

--- src/backend/utils/generateErrorMessage.ts
+++ src/backend/utils/generateErrorMessage.ts
@@ -3,6 +3,7 @@ import { ERROR_TYPE } from "./errorTypes";
 // Note: for security reasons if there is no ERROR_TYPE definition of the error,
 // the error won't be printed
 export function generateErrorResponse(error: unknown) {
+  console.log(error);
   let res = ERROR_TYPE.unknown
 
   Object.entries(ERROR_TYPE).forEach(([property, value]) => {
