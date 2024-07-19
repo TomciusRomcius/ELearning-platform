e3934551259cc24220fbe843b9088d964d341083
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-07-19 15:03:16 +0300

    Remove unnecessary import

--- src/app/(browsing)/layout.tsx
+++ src/app/(browsing)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
-import MainHeader from "@/frontend/ui/MainHeader/MainHeader";
 import SessionContainer from "@/frontend/ui/SessionContainer";
 import { isAdmin } from "@/backend/utils/isAdmin";
 import AdminHeader from "@/frontend/ui/AdminHeader";
